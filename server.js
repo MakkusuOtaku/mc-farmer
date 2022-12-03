0e8ae0340ab79f804958c956a6b02093b33f8265
Makkusu Otaku <makkusuotaku@gmail.com>
2022-12-03 11:54:04 +0800

    added the ability to start and expand farms

--- server.js
+++ server.js
@@ -1,14 +1,66 @@
 const express = require('express');
-const server = express();
+const mineflayer = require('mineflayer');
+const { Server } = require('socket.io');
 
+const app = express();
 const port = 80;
+const bots = [];
 
-server.get('/', async (request, response)=>{
+app.get('/', async (request, response)=>{
 	response.sendFile(`web/index.html`, { root: "./" });
 });
 
-server.use(express.static('./'));
+app.use(express.static('./web'));
 
-server.listen(port, ()=>{
-	console.log(`Running server on port ${port}.`);
+const server = app.listen(port);
+const io = new Server(server);
+
+const fakeChunkData = [
+	0, 0, 1, 1, 1, 0, 0,
+	0, 1, 1, 1, 1, 1, 0,
+	1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1,
+	0, 1, 1, 1, 1, 1, 0,
+	0, 0, 1, 1, 1, 0, 0,
+];
+
+io.on('connection', (socket)=>{
+	console.log('User connected.');
+
+	setTimeout(()=>{
+		io.emit('chunk-update', {
+			bot: null,
+			data: fakeChunkData,
+		});
+	}, 1000);
+
+	socket.on('disconnect', ()=>{
+		console.log('User disconnected.');
+	});
 });
+
+function createBot() {
+	let bot = mineflayer.createBot({
+		"name": `Machine_${bots.length}`,
+		"host": "localhost",
+	});
+
+	bot.once('spawn', ()=>{
+		scanSurroundings(bots[0]);
+	})
+
+	bots.push(bot);
+}
+
+function scanSurroundings(bot) {
+	let openList = [];
+	let closedList = [];
+
+	io.emit('chunk-update', {
+		bot: bot.username,
+		data: fakeChunkData,
+	});
+}
+
+createBot();
